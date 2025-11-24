a7126ece7e39e094c53408097cb268e659ca738b
darioPM2002 <a01785420@tec.mx>
2025-11-24 16:17:48 -0600

    Added files for visualizarion of model

---
+++ AgentsVisualization/assets/shaders/fs_phong_302.glsl
@@ -0,0 +1,43 @@
+#version 300 es
+precision highp float;
+
+in vec3 v_normal;
+in vec3 v_surfaceToLight;
+in vec3 v_surfaceToView;
+
+// Scene uniforms
+uniform vec4 u_ambientLight;
+uniform vec4 u_diffuseLight;
+uniform vec4 u_specularLight;
+
+// Model uniforms
+uniform vec4 u_ambientColor;
+uniform vec4 u_diffuseColor;
+uniform vec4 u_specularColor;
+uniform float u_shininess;
+
+out vec4 outColor;
+
+void main() {
+    // v_normal must be normalized because the shader will interpolate
+    // it for each fragment
+    vec3 normal = normalize(v_normal);
+
+    // Normalize the other incoming vectors
+    vec3 surfToLigthDirection = normalize(v_surfaceToLight);
+    vec3 surfToViewDirection = normalize(v_surfaceToView);
+
+    vec3 r = normalize (2.0 * dot (normal, surfToLigthDirection) * normal - surfToLigthDirection);
+
+    // CALCULATIONS FOR THE AMBIENT, DIFFUSE and SPECULAR COMPONENTS
+    float diffuse = max(dot(normal, surfToLigthDirection), 0.0);
+    float specular = pow(max(dot(r, surfToViewDirection), 0.0), u_shininess);
+
+    // Compute the three parts of the Phong lighting model
+    vec4 ambientColor = u_ambientLight * u_ambientColor;
+    vec4 diffuseColor = u_diffuseLight * u_diffuseColor * diffuse;
+    vec4 specularColor = u_specularLight * u_specularColor * specular;
+
+    // Use the color of the texture on the object
+    outColor = ambientColor + diffuseColor + specularColor;
+}
---
+++ AgentsVisualization/assets/shaders/vs_phong_302.glsl
@@ -0,0 +1,35 @@
+#version 300 es
+in vec4 a_position; // input | atributos unicos a cada vertice
+in vec3 a_normal;
+
+// Scene uniforms
+uniform vec3 u_lightWorldPosition; // atributos para todos los vertices
+uniform vec3 u_viewWorldPosition;
+
+// Model uniforms
+uniform mat4 u_world;
+uniform mat4 u_worldInverseTransform;
+uniform mat4 u_worldViewProjection;
+
+// Transformed normals
+out vec3 v_normal; // output
+out vec3 v_surfaceToLight;
+out vec3 v_surfaceToView;
+
+void main() {
+    // Transform the position of the vertices
+    gl_Position = u_worldViewProjection * a_position;
+
+    // Transform the normal vector along with the object
+    v_normal = mat3(u_worldInverseTransform) * a_normal;
+
+    // Get world position of the surface
+    vec3 surfaceWoldPosition = (u_world * a_position).xyz;
+
+
+    // Direction from the surface to the light
+    v_surfaceToLight = u_lightWorldPosition - surfaceWoldPosition;
+
+    // Direction from the surface to the view
+    v_surfaceToView = u_viewWorldPosition - surfaceWoldPosition;
+}
